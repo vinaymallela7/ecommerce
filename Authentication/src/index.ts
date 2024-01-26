f6e02ca559b7a576289f67b18a6e15e23092fe52
vinaymallela7 <vinay.glen4@gmail.com>
2024-01-26 21:46:59 +0530

    nginx and docker setup

--- Authentication/src/index.ts
+++ Authentication/src/index.ts
@@ -1,9 +1,13 @@
 import bodyParser from "body-parser";
-import express from "express";
+import express, { Request, Response } from "express";
 const app = express();
-const PORT = 8001;
+const PORT: Number = 8001;
 
 app.use(bodyParser.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Authentication service is up...');
+})
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
--- User/src/index.ts
+++ User/src/index.ts
@@ -1,9 +1,13 @@
 import bodyParser from "body-parser";
-import express from "express";
+import express, { Request, Response } from "express";
 const app = express();
-const PORT = 8000;
+const PORT: Number = 8000;
 
 app.use(bodyParser.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('User service is up...');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
